1a9999b89f49a59b8a0e12f4c43170107fd97696
Abhishek Sawant <sawantabhi091@gmail.com>
2021-07-06 14:17:45 +0530

    copied urls to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+
+  loginUrl:"https://fakestoreapi.com/auth/login",
+  usersUrl:"https://fakestoreapi.com/users",
+  allProductsUrl:"https://fakestoreapi.com/products",
+  productsUrl: "https://fakestoreapi.com/products/category/",
+  categoriesUrl:"https://fakestoreapi.com/products/categories/"
 };
